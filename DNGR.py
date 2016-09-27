1cb7b32d3255255dacbf44b1a3fb839ab6e0c53a
Mohammad Asif Khan <mak4086@gmail.com>
2016-09-27 14:54:41 +0300

    Add files via upload

---
+++ DNGR.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from keras.layers import Input, Dense, noise
+from keras.models import Model
+from keras import regularizers
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+import numpy as np
+import networkx as nx
+import pandas as pd
+from utils import DataGenerator, tsne
+from argparse import ArgumentParser, FileType, ArgumentDefaultsHelpFormatter
+import logging
+import sys
+import pdb
+
+
+def read_graph(filename,g_type):
+	with open('data/'+filename,'rb') as f:
+		if g_type == "undirected":
+			G = nx.read_weighted_edgelist(f)
+  		else:
+			G = nx.read_weighted_edgelist(f,create_using=nx.DiGraph())
+		node_idx = G.nodes()
+	adj_matrix = np.asarray(nx.adjacency_matrix(G, nodelist=None,weight='weight').todense())	
+	return adj_matrix, node_idx
+
+def scale_sim_mat(mat):
+	# Scale Matrix by row
+	mat  = mat - np.diag(np.diag(mat))
+	D_inv = np.diag(np.reciprocal(np.sum(mat,axis=0)))
+	mat = np.dot(D_inv,  mat)
+
+	return mat
+
+def random_surfing(adj_matrix,max_step,alpha):
+	# Random Surfing
+	nm_nodes = len(adj_matrix)
+	adj_matrix = scale_sim_mat(adj_matrix)
+	P0 = np.eye(nm_nodes, dtype='float32')
+	M = np.zeros((nm_nodes,nm_nodes),dtype='float32')
+	P = np.eye(nm_nodes, dtype='float32')
+	for i in range(0,max_step):
+		P = alpha*np.dot(P,adj_matrix) + (1-alpha)*P0
+		M = M + P
+
+	return M
+
+def PPMI_matrix(M):
+
+	M = scale_sim_mat(M)
+	nm_nodes = len(M)
+
+	col_s = np.sum(M, axis=0).reshape(1,nm_nodes)
+	row_s = np.sum(M, axis=1).reshape(nm_nodes,1)
+	D = np.sum(col_s)
+	rowcol_s = np.dot(row_s,col_s)
+	PPMI = np.log(np.divide(D*M,rowcol_s))
+	PPMI[np.isnan(PPMI)] = 0.0
+	PPMI[np.isinf(PPMI)] = 0.0
+	PPMI[np.isneginf(PPMI)] = 0.0
+	PPMI[PPMI<0] = 0.0
+
+	return PPMI
+
+
+def model(data, hidden_layers, hidden_neurons, output_file, validation_split=0.9):
+
+
+	train_n = int(validation_split * len(data))
+	batch_size = 50
+	train_data = data[:train_n,:]
+	val_data = data[train_n:,:]
+
+	input_sh = Input(shape=(data.shape[1],))
+	encoded = Dense(data.shape[1], activation='relu',activity_regularizer=regularizers.activity_l1l2(10e-5,10e-5))(input_sh)
+	encoded = noise.GaussianNoise(0.2)(encoded)
+	for i in range(hidden_layers):
+		encoded = Dense(hidden_neurons[i], activation='relu',activity_regularizer=regularizers.activity_l1l2(10e-5,10e-5))(encoded)
+		encoded = noise.GaussianNoise(0.2)(encoded)
+	for j in range(hidden_layers-1,-1,-1):
+		decoded = Dense(hidden_neurons[i], activation='relu',activity_regularizer=regularizers.activity_l1l2(10e-5,10e-5))(encoded)
+	decoded = Dense(data.shape[1], activation='sigmoid')(decoded)
+
+	autoencoder = Model(input=input_sh, output=decoded)
+	autoencoder.compile(optimizer='adadelta', loss='mse')
+
+	checkpointer = ModelCheckpoint(filepath='data/bestmodel' + output_file + ".hdf5", verbose=1, save_best_only=True)
+	earlystopper = EarlyStopping(monitor='val_loss', patience=15, verbose=1)
+
+	train_generator = DataGenerator(batch_size)
+	train_generator.fit(train_data, train_data)
+	val_generator = DataGenerator(batch_size)
+	val_generator.fit(val_data, val_data)
+
+	autoencoder.fit_generator(train_generator,
+		samples_per_epoch=len(train_data),
+		nb_epoch=100,
+		validation_data=val_generator,
+		nb_val_samples=len(val_data),
+		max_q_size=batch_size,
+		callbacks=[checkpointer, earlystopper])
+
+	enco = Model(input=input_sh, output=encoded)
+	enco.compile(optimizer='adadelta', loss='mse')
+	reprsn = enco.predict(data)
+
+	return reprsn
+
+
+def process_scripts(args):
+	
+	filename = args.input
+	graph_type = args.graph_type
+	Ksteps = args.random_surfing_steps
+	alpha = args.random_surfing_rate
+	output_file = args.output
+	hidden_layers = args.hidden_layers
+	hidden_neurons = args.neurons_hiddenlayer
+
+	data_mat, node_idx = read_graph(filename, graph_type)
+	data = random_surfing(data_mat, Ksteps, alpha)
+	data = PPMI_matrix(data)
+
+	reprsn = model(data_mat,hidden_layers,hidden_neurons,output_file)
+	data_reprsn = {'embedding':list(reprsn),'node_id':node_idx}
+	df = pd.DataFrame(data_reprsn)
+	df.to_pickle('data/'+output_file+'.pkl')
+
+def main():
+  parser = ArgumentParser('DNGR',
+                          formatter_class=ArgumentDefaultsHelpFormatter,
+                          conflict_handler='resolve')
+
+  parser.add_argument('--graph_type', default='undirected',
+                      help='Undirected or directed graph as edgelist')
+
+  parser.add_argument('--input', nargs='?', required=True,
+                      help='Input graph file')
+
+  parser.add_argument('--random_surfing_steps', default=10, type=int,
+                      help='Number of steps for random surfing')
+
+  parser.add_argument('--random_surfing_rate', default=0.98, type=float,
+                      help='alpha random surfing')
+
+  parser.add_argument('--output', required=True,
+                      help='Output representation file')
+
+  parser.add_argument('--hidden_layers', default=2, type=int,
+                      help='AutoEnocoder Layers')
+
+  parser.add_argument('--neurons_hiddenlayer', default=[128,32], type=list,
+                      help='Number of Neurons AE.')
+
+  args = parser.parse_args()
+
+  process_scripts(args)
+
+if __name__ == '__main__':
+	sys.exit(main())	
+	
---
+++ example.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+import networkx as nx
+from sklearn.cluster import KMeans
+import matplotlib.colors as colors
+from itertools import cycle
+import time
+import matplotlib.pyplot as plt
+import subprocess
+from utils import tsne
+import pdb
+import numpy as np
+from sklearn.metrics import normalized_mutual_info_score as nmi
+import scipy.io as sio
+
+
+def cluster(data,true_labels,n_clusters=3):
+
+	km = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
+	km.fit(data)
+
+	km_means_labels = km.labels_
+	km_means_cluster_centers = km.cluster_centers_
+	km_means_labels_unique = np.unique(km_means_labels)
+
+	colors_ = cycle(colors.cnames.keys())
+
+	initial_dim = np.shape(data)[1]
+	data_2 = tsne(data,2,initial_dim,30)
+
+	plt.figure(figsize=(12, 6))
+	plt.scatter(data_2[:,0],data_2[:,1], c=true_labels)
+	plt.title('True Labels')
+
+	return km_means_labels
+
+data_mat = sio.loadmat('data/wine_network.mat')
+labels = sio.loadmat('data/wine_label.mat')
+data_mat = data_mat['adjMat']
+labels = labels['wine_label']
+data_edge = nx.Graph(data_mat) 
+
+with open('data/wine.edgelist','wb') as f:
+	nx.write_weighted_edgelist(data_edge, f)
+
+subprocess.call('~/DNGR-Keras/DNGR.py --graph_type '+'undirected'+' --input '+'wine.edgelist'+' --output '+'representation',shell=True)
+
+df = pd.read_pickle('data/representation.pkl')
+reprsn = df['embedding'].values
+node_idx = df['node_id'].values
+reprsn = [np.asarray(row,dtype='float32') for row in reprsn]
+reprsn = np.array(reprsn, dtype='float32')
+true_labels = [labels[int(node)][0] for node in node_idx]
+true_labels = np.asarray(true_labels, dtype='int32')
+cluster(reprsn,true_labels, n_clusters=3)
+	
+plt.show()
+
+
+
+
