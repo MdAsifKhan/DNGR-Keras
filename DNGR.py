f90ccf75a3a0e59724607652c0d93169529eca8b
Mohammad Asif Khan <mak4086@gmail.com>
2017-01-20 15:46:57 +0300

    Update DNGR.py

--- DNGR.py
+++ DNGR.py
@@ -121,7 +121,7 @@ def process_scripts(args):
 	data = random_surfing(data_mat, Ksteps, alpha)
 	data = PPMI_matrix(data)
 
-	reprsn = model(data_mat,hidden_layers,hidden_neurons,output_file)
+	reprsn = model(data,hidden_layers,hidden_neurons,output_file)
 	data_reprsn = {'embedding':list(reprsn),'node_id':node_idx}
 	df = pd.DataFrame(data_reprsn)
 	df.to_pickle('data/'+output_file+'.pkl')
